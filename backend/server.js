e621b3e7491b9d48b1de521dc4d4309f8d1f2e71
hakyung4 <hakyung4@illinois.edu>
2022-06-02 11:55:05 -0500

    frontend user authentication login logout register done

--- backend/server.js
+++ backend/server.js
@@ -10,14 +10,13 @@ connectDB()
 
 const app = express()
 
-app.use(cors)
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.use('/api/goals', require('./routes/goalRoutes'))
 app.use('/api/users', require('./routes/userRoutes'))
-
+app.use(cors)
 app.use(errorHandler)
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
