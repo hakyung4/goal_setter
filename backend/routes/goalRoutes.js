b6cb5a47e7a97e930d974163e196df7bfd3a97ce
hakyung4 <hakyung4@illinois.edu>
2022-06-01 21:49:18 -0500

    REST API for goals done

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const { getGoals, setGoals, updateGoals, deleteGoals } = require('../controllers/goalController')
+const { getGoals, setGoal, updateGoal, deleteGoal } = require('../controllers/goalController')
 
-router.route('/').get(getGoals).post(setGoals)
+router.route('/').get(getGoals).post(setGoal)
 
-router.route('/:id').delete(deleteGoals).put(updateGoals)
+router.route('/:id').delete(deleteGoal).put(updateGoal)
 
 module.exports = router
