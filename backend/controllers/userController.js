d0c1a4746b9ca24553aa1dd3be1c422b647f0adc
hakyung4 <hakyung4@illinois.edu>
2022-06-01 23:14:44 -0500

    add user model and user routes

---
+++ backend/controllers/userController.js
@@ -0,0 +1,78 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+const asyncHandler = require('express-async-handler')
+const User = require('../models/userModel')
+
+// @description Register new user
+// @route POST /api/users
+// @access Public
+const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body
+
+    if (!name || !email || !password) {
+        res.status(400)
+        throw new Error('Please fill in all required fields')
+    }
+
+    // Check if the user exists
+    const userExists = await User.findOne({email})
+
+    if (userExists) {
+        res.status(400)
+        throw new Error('User already exists!')
+    }
+
+    // hash password using bcrypt
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    // Create user
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword
+    })
+
+    if (user) {
+        res.status(201).json({
+            _id: user.id,
+            name: user.name,
+            email: user.email
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
+})
+
+// @description Authenticate a user
+// @route POST /api/users/login
+// @access Public
+const loginUser = asyncHandler(async (req, res) => {
+    const {email, password} = req.body
+
+    // Check the email
+    const user = await User.findOne({email})
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+        res.json({
+            _id: user.id,
+            name: user.name,
+            email: user.email
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid credentials')
+    }
+})
+
+// @description Get user data
+// @route GET /api/users/me
+// @access Public
+const getMe = asyncHandler(async (req, res) => {
+    res.json({message: 'User data'})
+})
+
+module.exports = {
+    registerUser, loginUser, getMe
+}
--- backend/models/goalModel.js
+++ backend/models/goalModel.js
@@ -1,10 +1,16 @@
 const mongoose = require('mongoose')
 
 const goalSchema = mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId, 
+        require: true,
+        ref: 'User',
+    },
     text: {
         type: String,
         required: [true, 'Please add a text value']
-    }
+    },
+
 }, {
     timestamps: true
 })
