8559fb70197f19a1f4450dfd290012ec869e3b21
hakyung4 <hakyung4@illinois.edu>
2022-06-02 10:09:29 -0500

    add some UI

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,16 +1,14 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
-import { FaUser } from 'react-icons/fa'
+import { FaSign, FaSignInAlt } from 'react-icons/fa'
 
 function Login() {
     const [ formData, setFormData] = useState({
-        name: '',
         email: '',
         password: '',
-        password2: ''
     })
 
-    const {name, email, password, password2} = formData
+    const { email, password } = formData
 
     const onChange = (e) => {
         setFormData((prevState) => ({
@@ -27,24 +25,18 @@ function Login() {
     <>
     <section className='heading'>
         <h1>
-            <FaUser /> Register
+            <FaSignInAlt /> Login
         </h1>
-        <p>Please create an account</p>
+        <p>Login and start setting goals!</p>
     </section>
 
     <section className='form'>
         <form onSubmit={onSubmit}>
             <div className='form-group'>
-                <input type='text' className='form-control' id='name' name='name' value={name} placeholder='Enter your name' onChange={onChange} />
-            </div>
-            <div className='form-group'>
                 <input type='email' className='form-control' id='email' name='email' value={email} placeholder='Enter your email' onChange={onChange} />
             </div>
             <div className='form-group'>
                 <input type='password' className='form-control' id='password' name='password' value={password} placeholder='Enter password' onChange={onChange} />
-            </div>
-            <div className='form-group'>
-                <input type='password' className='form-control' id='password2' name='password2' value={password2} placeholder='Confirm password' onChange={onChange} />
             </div>
             <div className='form-group'>
                 <button type='submit' className='btn btn-block'>
